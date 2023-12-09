3e408b9aef7e93ebc8fbc7ec80cf81dd40d65258
geromedw <geromedw@gmail.com>
2023-12-09 14:59:10 +0100

    test

--- model.py
+++ model.py
@@ -205,7 +205,7 @@ def set_input(interpreter, data):
     """Copies data to input tensor."""
     interpreter_shape = interpreter.get_input_details()[0]['shape']
     data_size = np.prod(data.shape)
-    target_shape_size = np.prod(interpreter_shape[1:3])
+    target_shape_size = np.prod(interpreter_shape[1:2])
 
     print("Size of Data Array:", data_size)
     print("Size of Target Shape:", target_shape_size)

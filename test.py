fb8f1c15f1251af690dcd4dc43b678b418bf4dc6
geromedw <geromedw@gmail.com>
2023-12-09 16:19:05 +0100

    dfqsf

--- test.py
+++ test.py
@@ -40,7 +40,7 @@ train_ds, val_ds = tf.keras.utils.audio_dataset_from_directory(
     batch_size=64,
     validation_split=0.2,
     seed=0,
-    output_sequence_length=1600,
+    output_sequence_length=16000,
     subset='both')
 
 label_names = np.array(train_ds.class_names)

1994b402d9d0d7ffdfbb6ac056ca0567474735e1
geromedw <geromedw@gmail.com>
2023-12-17 13:54:37 +0100

    command matplotlib kevin

--- Kevin/TestFileTreshold.py
+++ Kevin/TestFileTreshold.py
@@ -1,7 +1,7 @@
 import pyaudio
 import pathlib
-import numpy as np
-import matplotlib.pyplot as plt
+""" import numpy as np
+import matplotlib.pyplot as plt """
 
 import tensorflow as tf
 from keras import models

ea475869b3aea9b92439d95f8ad72ed9d1cad49e
geromedw <geromedw@gmail.com>
2023-12-17 21:03:09 +0100

    test

--- modeluserV2.py
+++ modeluserV2.py
@@ -90,10 +90,10 @@ except KeyboardInterrupt:
 Threshold = 20
 
 SHORT_NORMALIZE = (1.0/32768.0)
-chunk = 1024
+chunk = 2**9
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 44100
+RATE = 48000
 swidth = 2
 
 TIMEOUT_LENGTH = 2

919e5c68bca3d3aaa67b5404c23098f1c889cbd7
geromedw <geromedw@gmail.com>
2023-12-17 19:05:17 +0100

    test

--- modeluserV2.py
+++ modeluserV2.py
@@ -5,6 +5,12 @@ import pyaudio
 import wave
 import pathlib
 
+import pyaudio
+import math
+import struct
+import wave
+import time
+import os
 
 def get_spectrogram(waveform):
   spectrogram = tf.signal.stft(waveform, frame_length=255, frame_step=128)
@@ -13,14 +19,9 @@ def get_spectrogram(waveform):
   return spectrogram
 
 
-def checkModel(data):
+def checkModel():
 
-    waveform = wave.open('test.wav', 'w')
-    waveform.setnchannels(1)
-    waveform.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
-    waveform.setframerate(16000)
-    waveform.writeframes(b''.join(data))
-    waveform.close()
+
 
     x = 'test.wav'
     x = tf.io.read_file(str(x))
@@ -68,7 +69,7 @@ stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, outpu
 player = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, output=True, frames_per_buffer=CHUNK)
 
 
-try:
+""" try:
     while True:
         arrayFrames=[]
         for i in range(int(LEN * RATE / CHUNK)):  # go for a LEN seconds
@@ -82,4 +83,85 @@ try:
 except KeyboardInterrupt:
     stream.stop_stream()
     stream.close()
-    p.terminate()
+    p.terminate() """
+
+
+
+
+Threshold = 20
+
+SHORT_NORMALIZE = (1.0/32768.0)
+chunk = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+swidth = 2
+
+TIMEOUT_LENGTH = 2
+
+class Recorder:
+
+    @staticmethod
+    def rms(frame):
+        count = len(frame) / swidth
+        format = "%dh" % (count)
+        shorts = struct.unpack(format, frame)
+
+        sum_squares = 0.0
+        for sample in shorts:
+            n = sample * SHORT_NORMALIZE
+            sum_squares += n * n
+        rms = math.pow(sum_squares / count, 0.5)
+
+        return rms * 1000
+
+    def __init__(self):
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=FORMAT,
+                                  channels=CHANNELS,
+                                  rate=RATE,
+                                  input=True,
+                                  output=True,
+                                  frames_per_buffer=chunk)
+
+    def record(self):
+        print('Noise detected, recording beginning')
+        rec = []
+        current = time.time()
+        end = time.time() + TIMEOUT_LENGTH
+
+        while current <= end:
+
+            data = self.stream.read(chunk)
+            if self.rms(data) >= Threshold: end = time.time() + TIMEOUT_LENGTH
+
+            current = time.time()
+            rec.append(data)
+        self.write(b''.join(rec))
+
+    def write(self, recording):
+        n_files = len(os.listdir("test"))
+
+        filename = os.path.join("test", 'test.wav'.format(n_files))
+
+        wf = wave.open(filename, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(self.p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(recording)
+        wf.close()
+        print('Written to file: {}'.format(filename))
+        print('Returning to listening')
+        checkModel()
+
+    def listen(self):
+        print('Listening beginning')
+        while True:
+            input = self.stream.read(chunk)
+            rms_val = self.rms(input)
+            if rms_val > Threshold:
+                self.record()
+
+a = Recorder()
+
+a.listen()

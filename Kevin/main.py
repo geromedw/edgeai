1a259e7108d65dd632e53a8feb28fba04b480c8b
LaheyKevin <91462775+LaheyKevin@users.noreply.github.com>
2023-12-20 21:20:19 +0100

    Update main.py

--- Kevin/main.py
+++ Kevin/main.py
@@ -17,14 +17,22 @@ interpreter.allocate_tensors()
 
 def checkModel(data):
 
-    prediction = interpreter(data)
-    x_labels = ['boom', 'hond', 'kat', 'uitlaat', 'vrede', 'water']
-    prediction = tf.nn.softmax(prediction[0])
+    input_tensor_index = interpreter.get_input_details()[0]['index']
+    interpreter.tensor(input_tensor_index)()[0] = data
 
-    #print(prediction.numpy())
-    for i in range(len(x_labels)):
-        if prediction[i].numpy() == max(prediction.numpy()):
-            print(f"{x_labels[i]}: {max(prediction.numpy())}%")
+    interpreter.invoke()
+    
+    output_tensor_index = interpreter.get_output_details()[0]['index']
+    output_data = interpreter.tensor(output_tensor_index)()[0]
+    print(output_data)
+    #prediction = interpreter(data)
+    # x_labels = ['boom', 'hond', 'kat', 'uitlaat', 'vrede', 'water']
+    # prediction = tf.nn.softmax(prediction[0])
+
+    # #print(prediction.numpy())
+    # for i in range(len(x_labels)):
+    #     if prediction[i].numpy() == max(prediction.numpy()):
+    #         print(f"{x_labels[i]}: {max(prediction.numpy())}%")
 
 #
 while True:

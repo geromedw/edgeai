b7ea3fbcdd62c64d5581c297f014be4e01a65a89
LaheyKevin <91462775+LaheyKevin@users.noreply.github.com>
2023-12-20 21:38:34 +0100

    Update main.py

--- Kevin/main.py
+++ Kevin/main.py
@@ -9,10 +9,11 @@ import wave
 
 from functions import *
 
-import tflite_runtime.interpreter as tflite
+#import tflite_runtime.interpreter as tflite
+
 
 #imported = models.load_model("saved1.keras")
-interpreter = tflite.Interpreter("model.tflite")
+interpreter = tf.lite.Interpreter("model.tflite")
 interpreter.allocate_tensors()
 
 def checkModel(data):
@@ -25,8 +26,11 @@ def checkModel(data):
     output_tensor_index = interpreter.get_output_details()[0]['index']
     output_data = interpreter.tensor(output_tensor_index)()[0]
     print(output_data)
+    
     #prediction = interpreter(data)
-    # x_labels = ['boom', 'hond', 'kat', 'uitlaat', 'vrede', 'water']
+    x_labels = ['boom', 'hond', 'kat', 'uitlaat', 'vrede', 'water']
+
+    print(x_labels[np.argmax(output_data)])
     # prediction = tf.nn.softmax(prediction[0])
 
     # #print(prediction.numpy())

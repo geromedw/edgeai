c3c35438289e3cda526a5836467237fd71af9e2c
LaheyKevin <91462775+LaheyKevin@users.noreply.github.com>
2023-12-22 10:44:41 +0100

    Create bob.py

---
+++ Kevin/bob.py
@@ -0,0 +1,79 @@
+import pyaudio
+import pathlib
+import numpy as np
+import tensorflow as tf
+
+from keras import models
+
+import wave
+
+#FUNCTIES
+def get_spectrogram(waveform):
+  spectrogram = tf.signal.stft(waveform, frame_length=255, frame_step=128)
+  spectrogram = tf.abs(spectrogram)
+  spectrogram = spectrogram[..., tf.newaxis]
+  return spectrogram
+
+#MODEL
+
+interpreter = tf.lite.Interpreter("EDGEAI/model.tflite")
+interpreter.allocate_tensors()
+#imported.summary()
+
+def checkModel(data):
+    waveform = wave.open(f"test/test.wav", "w")
+    waveform.setnchannels(1)
+    waveform.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
+    waveform.setframerate(16000)
+    waveform.writeframes(b''.join(data))
+    waveform.close()
+
+    x = pathlib.Path("test/")/'test.wav'
+    x = tf.io.read_file(str(x))
+    x, sample_rate = tf.audio.decode_wav(x, desired_channels=1, desired_samples=16000)
+    x = tf.squeeze(x, axis=-1)
+
+    x = get_spectrogram(x)
+    x = x[tf.newaxis,...]
+
+    input_tensor_index = interpreter.get_input_details()[0]['index']
+    interpreter.tensor(input_tensor_index)()[0] = x
+
+    interpreter.invoke()
+    
+    output_tensor_index = interpreter.get_output_details()[0]['index']
+    output_data = interpreter.tensor(output_tensor_index)()[0]
+    
+    print(output_data)
+    
+    #prediction = interpreter(data)
+    x_labels = ['boom', 'deur', 'hond', 'tafel', 'vrede', 'water']
+
+    print(x_labels[np.argmax(output_data)])
+
+
+#AUDIO
+CHUNK = 1024
+RATE = 16000
+LEN = 1
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index=0)
+#player = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, output=True, frames_per_buffer=CHUNK)
+
+try:
+    while True:
+        arrayFrames=[]
+        for i in range(int(LEN * RATE / CHUNK)):  # go for a LEN seconds
+            audio = stream.read(CHUNK)
+            #data = np.fromstring(audio, dtype=np.int16)
+            arrayFrames.append(audio)
+            #player.write(data, CHUNK)
+        checkModel(arrayFrames)
+        arrayFrames.clear()
+
+except KeyboardInterrupt:
+    stream.stop_stream()
+    stream.close()
+    p.terminate()

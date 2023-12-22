709afd89d4a0bda33b2e67731fabed9b8cf08abd
LaheyKevin <91462775+LaheyKevin@users.noreply.github.com>
2023-12-22 10:37:35 +0100

    Update main.py

--- Kevin/main.py
+++ Kevin/main.py
@@ -1,19 +1,13 @@
-import pyaudio
-import pathlib
+
 import numpy as np
 import tensorflow as tf
-
-from keras import models
-
-import wave
 
 from functions import *
 
 #import tflite_runtime.interpreter as tflite
 
-
 #imported = models.load_model("saved1.keras")
-interpreter = tf.lite.Interpreter("model.tflite")
+interpreter = tf.lite.Interpreter("EDGEAI/model.tflite")
 interpreter.allocate_tensors()
 
 def checkModel(data):
@@ -25,10 +19,11 @@ def checkModel(data):
     
     output_tensor_index = interpreter.get_output_details()[0]['index']
     output_data = interpreter.tensor(output_tensor_index)()[0]
+    
     print(output_data)
     
     #prediction = interpreter(data)
-    x_labels = ['boom', 'hond', 'kat', 'uitlaat', 'vrede', 'water']
+    x_labels = ['boom', 'deur', 'hond', 'tafel', 'vrede', 'water']
 
     print(x_labels[np.argmax(output_data)])
     # prediction = tf.nn.softmax(prediction[0])
@@ -38,9 +33,8 @@ def checkModel(data):
     #     if prediction[i].numpy() == max(prediction.numpy()):
     #         print(f"{x_labels[i]}: {max(prediction.numpy())}%")
 
-#
+
 while True:
     audio = get_audio()
-    print(audio.size)
     spectrogram = preprocess_audiobuffer(audio, (124,129,1))
     checkModel(spectrogram)
